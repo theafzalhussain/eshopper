40ab316b20fd67761775a2df8ad36c7da769bf05
theafzalhussain <152854253+theafzalhussain@users.noreply.github.com>
2026-02-21 12:45:15 +0530

    Fixed cart NaN and premium product page

--- src/Store/Sagas/RootSaga.jsx
+++ src/Store/Sagas/RootSaga.jsx
@@ -1,4 +1,6 @@
-import { all } from "redux-saga/effects"
+import { all, fork } from "redux-saga/effects"
+
+// Sabhi Sagas ko import karein
 import { maincategorySaga } from "./MaincategorySaga"
 import { subcategorySaga } from "./SubcategorySaga"
 import { brandSaga } from "./BrandSaga"
@@ -10,17 +12,22 @@ import { checkoutSaga } from "./CheckoutSaga"
 import { contactSaga } from "./ContactSaga"
 import { newslatterSaga } from "./NewslatterSaga"
 
+/**
+ * RootSaga: Ye aapki poori application ka engine hai.
+ * 'fork' ka use karne se saari sagas background mein parallel chalti hain.
+ * Isse loading speed fast ho jati hai aur app crash nahi hota.
+ */
 export default function* RootSaga() {
     yield all([
-        maincategorySaga(),
-        subcategorySaga(),
-        brandSaga(),
-        productSaga(),
-        userSaga(),
-        cartSaga(),
-        wishlistSaga(),
-        checkoutSaga(),
-        contactSaga(),
-        newslatterSaga()
+        fork(maincategorySaga),
+        fork(subcategorySaga),
+        fork(brandSaga),
+        fork(productSaga),
+        fork(userSaga),
+        fork(cartSaga),
+        fork(wishlistSaga),
+        fork(checkoutSaga),
+        fork(contactSaga),
+        fork(newslatterSaga)
     ])
 }
