66c2aafe82056842b6aa32775a15f10a00735261
theafzalhussain <152854253+theafzalhussain@users.noreply.github.com>
2026-03-15 12:13:22 +0530

    Fixed railway workflow build error

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ const { sendTransactionalEmail } = require('./src/utils/email');
 const sendEmail = sendTransactionalEmail;
 // ===== EMAIL UTILITY (Brevo)
 require('./models/OTPRecord');
-require('./models/User');
+const User = require('./models/User');
 const authRoutes = require('./routes/authRoutes');
 // ...existing code...
 // ...existing code...
