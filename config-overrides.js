bd6ea3c4c7aa577f97f255232f697f435b019c0a
theafzalhussain <152854253+theafzalhussain@users.noreply.github.com>
2026-03-24 13:10:15 +0530

    Fixed railway workflow build error

---
+++ config-overrides.js
@@ -0,0 +1,20 @@
+const webpack = require('webpack');
+
+module.exports = {
+  // ...existing config (CRA will merge this)
+  resolve: {
+    fallback: {
+      http: require.resolve('stream-http'),
+      https: require.resolve('https-browserify'),
+      zlib: require.resolve('browserify-zlib'),
+      stream: require.resolve('stream-browserify'),
+      crypto: require.resolve('crypto-browserify'),
+    },
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      process: 'process/browser',
+      Buffer: ['buffer', 'Buffer'],
+    }),
+  ],
+};
